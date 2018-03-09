82a6aaab989f79bd50af10311ba9710a6fce0d9c
線上讀書會 <polo13999@gmail.com>
2018-03-09 12:31:08 +0800

    update

---
+++ functions/next/bundles/pages/about.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/about', function() {
+            var comp = module.exports=webpackJsonp([5],{336:function(e,t,u){e.exports=u(337)},337:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(7),o=n(l),r=u(168),a=n(r);t.default=function(){return o.default.createElement(a.default,null,o.default.createElement("p",null,"About Page"))}}},[336]);
+            return { page: comp.default }
+          })
+        
---
+++ functions/next/bundles/pages/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/', function() {
+            var comp = module.exports=webpackJsonp([4],{351:function(e,t,u){e.exports=u(352)},352:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(7),r=n(l),a=u(168),o=n(a);t.default=function(){return r.default.createElement(o.default,null,r.default.createElement("p",null,"Index Page"))}}},[351]);
+            return { page: comp.default }
+          })
+        
